35ab2aa440204f9cc38d86918f2f8fc37640eabc
Aleks-ey <Akhukhua@yahoo.com>
2023-04-25 21:40:18 -0600

    icon change and commented out mission page from appjs

--- hotbeam_website/src/App.js
+++ hotbeam_website/src/App.js
@@ -6,7 +6,7 @@ import Layout from './components/Layout/layout_index';
 import Home from './components/Home/home_index';
 import Services from './components/Services/services_index';
 import ContactUs from './components/Contact_Us/contact_index';
-import OurMission from './components/Our_Mission/mission_index';
+// import OurMission from './components/Our_Mission/mission_index';
 import MeetTheDJs from './components/Meet_the_DJs/meet_index';
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/meet" element={<MeetTheDJs />} />
-        <Route path="/mission" element={<OurMission />} />
+        {/* <Route path="/mission" element={<OurMission />} /> */}
         <Route path="/contact" element={<ContactUs />} />
       </Route>
     </Routes>
